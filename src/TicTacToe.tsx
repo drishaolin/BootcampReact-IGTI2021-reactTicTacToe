12418fbdc8bfdd0e9de5b1419ac76be1870c9b98
Adriana Mucciolo <drishaolin@gmail.com>
2021-08-16 13:47:00 -0300

    create function ticTacToeReducer and const store

--- src/TicTacToe.tsx
+++ src/TicTacToe.tsx
@@ -1,9 +1,43 @@
+import { Action, configureStore, PayloadAction } from "@reduxjs/toolkit";
+
 type CellValue = "X" | "O" | "";
 interface ITicTacToeState {
     nextPlayer: "X" | "O",
     board: CellValue[][],
 }
 
+const initialState: ITicTacToeState = {
+    nextPlayer: "X",
+    board: [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+    ]
+} 
+
+type ActionPlay = PayloadAction<{i: number, j: number}, "play">;
+type ActionReset = Action<"reset">;
+
+function ticTacToeReducer (state = initialState, action: ActionPlay | ActionReset) : ITicTacToeState {
+    switch(action.type) {
+        case "play":
+            const board = state.board.map(row => row.map(cell =>(cell)));
+            return {
+                nextPlayer: state.nextPlayer === "X" ? "O" : "X",
+                board,
+            }
+        case "reset":
+    }
+    return state;
+}
+
+const store = configureStore({
+    reducer: {
+        ticTacToe: ticTacToeReducer,
+    },
+});
+
+console.log(store.getState());
 
 export default function TicTacToe() {
     const state: ITicTacToeState = {
