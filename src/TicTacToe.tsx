9e4808fe0bb7ed52d133492e0e93ac908c710192
Adriana Mucciolo <drishaolin@gmail.com>
2021-08-16 13:21:13 -0300

    logic for populate TicTacToe table

--- src/TicTacToe.tsx
+++ src/TicTacToe.tsx
@@ -1,24 +1,31 @@
+type CellValue = "X" | "O" | "";
+interface ITicTacToeState {
+    nextPlayer: "X" | "O",
+    board: CellValue[][],
+}
+
+
 export default function TicTacToe() {
+    const state: ITicTacToeState = {
+        nextPlayer: "X",
+        board: [
+            ["", "X", ""],
+            ["", "", ""],
+            ["O", "", ""],
+        ]
+    } 
     return (
         <div className="ticTacToe">
-            <div>Aguardando a jogada de X</div>
+            <div>Aguardando a jogada de {state.nextPlayer}</div>
             <table>
                 <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                    {state.board.map((row, i) => (
+                    <tr key={i}>
+                        {row.map((cell, j) => (
+                        <td key={j}>{cell}</td>
+                        ))}
                     </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                    ))}
                 </tbody>
             </table>
             <button>Reiniciar partida</button>
