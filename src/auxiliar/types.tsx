e28efde707465d38b78afb82605fb1169d320859
Adriana Mucciolo <drishaolin@gmail.com>
2021-08-24 10:20:04 -0300

    remove wrong comment in types.tsx

--- src/auxiliar/types.tsx
+++ src/auxiliar/types.tsx
@@ -18,7 +18,6 @@ export type ActionPlay = PayloadAction<{i: number, j: number}>;
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
